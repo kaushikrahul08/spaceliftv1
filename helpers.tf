66481a10d4f1e456ae37f36f13939ec7622bf771
RahulSharma <66063169+kaushikrahul08@users.noreply.github.com>
2024-11-19 10:52:52 -0500

    Update helpers.tf

--- helpers.tf
+++ helpers.tf
@@ -2,17 +2,11 @@ module "naming" {
   source            = "./modules/naming"
   rg_configurations = [
     {
-      subscription_type = "app",
+      environment       = "${var.environment}"
+      subscription_type = "lux",
       rg_type           = "shrd"
       instance_number   = "001"
     }
   ]
-  vnet_configurations = [
-  {
-    subscription_type = "app",
-    rg_type           = "shrd"
-    instance_number   = "001"
-  }
-]
 
 }
