e405da28b84d674f1519dfdbd15f567012077250
RahulSharma <66063169+kaushikrahul08@users.noreply.github.com>
2024-11-19 11:29:05 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,66 +1,57 @@
+################################################################################
+## REQUIRED PROVIDERS
+################################################################################
+
 terraform {
   required_version = "~> 1.8.2"
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.114"
-    }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.47.0"
+      version = "~> 2.50.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.102.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.2.3"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5"
+      version = "~> 3.6.1"
     }
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "4.39.0"
-    }
-    azapi = {
-      source  = "Azure/azapi"
-      version = "~> 1.9"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = "~> 0.12"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.2.2"
+    spacelift = {
+      source  = "spacelift-io/spacelift"
+      version = "~> 1.16.1"
     }
   }
 }
+
+################################################################################
+## AZURERM
+################################################################################
 
 provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = true
-    }
-  }
-}
-# provider "azurerm" {
-#   subscription_id = local.ops_subscription_id
-#   alias           = "ops"
-#   features {
-#     resource_group {
-#       prevent_deletion_if_contains_resources = true
-#     }
-#   }
-# }
-# provider "azurerm" {
-#   subscription_id = local.connectivity_subscription_id
-#   alias           = "connectivity"
-#   features {
-#     resource_group {
-#       prevent_deletion_if_contains_resources = true
-#     }
-#   }
-# }
-
-
-## RPS set to 3 to reduce rateLimtiing (Default 4)
-provider "cloudflare" {
-  #rps = 3
+  features {}
 }
 
+################################################################################
+## AZURE AD
+################################################################################
+
+provider "azuread" {
+
+}
+
+################################################################################
+## GITHUB
+################################################################################
+
+
+################################################################################
+## PROVIDER: SPACELIFT
+################################################################################
+
+provider "spacelift" {
+}
