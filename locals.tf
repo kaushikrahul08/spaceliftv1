25fab357b4e34a6a388432dcf4f9068a150f18a6
RahulSharma <66063169+kaushikrahul08@users.noreply.github.com>
2024-11-19 10:52:26 -0500

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -4,8 +4,6 @@ locals {
         compute_snet    = "ComputeSubnet"
         pvtlink_snet    = "PrivatelinkSubnet"
         resource_group_log_alert = ["log"]
-        log_alert_location       = "centralus"
-        location_short_name      = "cus"
         # kv_dns_zone_group_name   = "kvdnsgrp${var.application_name}${var.subscription_type}${var.instance_number}"
         # sa_dns_zone_group_name   = "sadnsgrp${var.application_name}${var.subscription_type}${var.instance_number}"
         # sa_pes_conn_name         = "sapeconn${var.application_name}${var.subscription_type}${var.instance_number}"
@@ -14,8 +12,6 @@ locals {
         subresource_names_kv     = ["Vault"]
         # ops_subscription_id      = ""
         # connectivity_subscription_id = ""
-        lux_subscription_id          = "bebb6b61-5ac1-4f45-9e8d-b64f895f94e9"
-
   }
 }
 
@@ -31,9 +27,13 @@ locals {
 }
 
 locals {
-  dev_network = {
-    app_pvtlink_subnet_address_prefixes = ["10.20.1.64/26"]
-    app_vnet_address_space              = ["10.20.1.0/24"]
-    app_cmp_subnet_address_prefixes     = ["10.20.1.0/26"]
-  }
+  dv = {
+    network = {
+      app_vnet_address_space              = ["10.20.1.0/24"]
+      app_pvtlink_subnet_address_prefixes = ["10.20.1.64/26"]
+      app_cmp_subnet_address_prefixes     = ["10.20.1.0/26"]
+      }
+    lux_subscription_id                   = "bebb6b61-5ac1-4f45-9e8d-b64f895f94e9"
+
+    }
 }
