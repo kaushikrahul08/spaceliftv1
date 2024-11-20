1488081dc29c818efcc6da5d93cacb6bb4664238
RahulSharma <66063169+kaushikrahul08@users.noreply.github.com>
2024-11-20 15:27:00 -0500

    Update helpers.tf

--- helpers.tf
+++ helpers.tf
@@ -1,3 +1,33 @@
+  region = { for x in flatten([
+    for region_key, region in local.regions_manifest : [
+      for environment_key, environment in region.environments : merge(local.regions_defaults, region, environment, {
+        app_region                           = region_key
+        app_environment                      = environment_key
+        environments                         = null
+        regions                              = null
+      })
+    ]
+  ]) : "${x.app_region}_${x.app_environment}" => x }["${var.APP_REGION}_${var.APP_ENVIRONMENT}"]
+
+
+
+
+
+module "naming" {
+  source            = "./modules/naming"
+  rg_configurations = [
+    {
+      environment       = "${var.ENVIRONMENT}"
+      subscription_type = "lux",
+      rg_type           = "shrd"
+      instance_number   = "001"
+    }
+  ]
+
+}
+
+
+
 module "naming" {
   source            = "./modules/naming"
   rg_configurations = [
