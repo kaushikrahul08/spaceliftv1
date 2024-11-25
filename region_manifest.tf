d3fb459d1930e58bc2ffdd6b40d621926dac48c1
RahulSharma <66063169+kaushikrahul08@users.noreply.github.com>
2024-11-25 13:35:20 -0500

    Update region_manifest.tf

--- region_manifest.tf
+++ region_manifest.tf
@@ -8,7 +8,7 @@ locals {
   regions_manifest = {
     euz = {
       azure_region        = "eastus"
-      enable_rg           = false
+      enable_rg           = true
       environments = {
         dv = {
           environment     = "${var.APP_ENVIRONMENT}"
