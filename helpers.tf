5fb57983c126d386cf3388c45048e6fccf49e83b
Rahul Sharma <Rahul.Sharma@escalent.co>
2024-11-29 12:49:02 -0500

    testing tag

--- helpers.tf
+++ helpers.tf
@@ -25,7 +25,7 @@ module "naming" {
     {
       #environment       = "${var.APP_ENVIRONMENT}"
       subscription_type = "lux",
-      rg_type           = "app02"
+      rg_type           = "test"
       instance_number   = "002"
     }
   ]
